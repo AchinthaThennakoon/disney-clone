0e08aadfddcf9a8e8bdc5a159d8405519b2882c7
Achintha Thennakoon <achinthathennakoon97@gmail.com>
2024-06-07 23:24:15 +0530

    correct getTrendingMoviesurl

--- src/services/gloabalApi.js
+++ src/services/gloabalApi.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const movieBaseUrl = "https://api.themoviedb.org/3/";
 const api_key = "fa6a156b8b7f81648a0540fb0adac599";
 
-const getTrendingMovies = axios.get(this.movieBaseUrl + "/trending/all/day?api_key" + this.api_key);
+const getTrendingMovies = axios.get(movieBaseUrl + "trending/all/day?api_key=" + api_key);
 
 export default {
     getTrendingMovies
