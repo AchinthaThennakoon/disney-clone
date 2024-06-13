ac9bebfc5e1fd15a8ef176bef9294ae88fb903be
Achintha Thennakoon <achinthathennakoon97@gmail.com>
2024-06-13 22:58:11 +0530

    add api call get movies by genre id

--- src/services/gloabalApi.js
+++ src/services/gloabalApi.js
@@ -5,6 +5,12 @@ const api_key = "fa6a156b8b7f81648a0540fb0adac599";
 
 const getTrendingMovies = axios.get(movieBaseUrl + "trending/all/day?api_key=" + api_key);
 
+const getMoviesByGenreId = (id) => {
+    return axios.get(movieBaseUrl + "&with_genres" + id)
+};
+
+
 export default {
-    getTrendingMovies
+    getTrendingMovies,
+    getMoviesByGenreId
 }
